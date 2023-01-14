3b64301d78d96df6f97003ef94ed3074a72e710e
Sacoya <sacoyaengineer@gmail.com>
2023-01-14 01:08:21 -0500

    add click event

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("hello");
+document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    console.log(e.target.search.value)
+})
+
+const input = document.getElementById('new_search');
+input.addEventListener('click', function() {
+  alert('Lets Explore Makeup !!!');
+})
