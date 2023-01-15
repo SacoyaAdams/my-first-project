5496fe108084c19c243374de0aa8b62000d42d86
Sacoya <sacoyaengineer@gmail.com>
2023-01-15 18:39:56 -0500

    forEach method

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
+const input = document.getElementById('new_search');
 document.querySelector('form').addEventListener('submit', (e) => {
     e.preventDefault()
-    console.log(e.target.search.value)
-})
-
-const input = document.getElementById('new_search');
 input.addEventListener('click', function() {
   alert('Lets Explore Makeup !!!');
 })
+    console.log(e.target.search.value)
+    fetch(`http://makeup-api.herokuapp.com/api/v1/products.json?brand=${e.target.search.value}`)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+      data.forEach(dataItem => {
+        let image = document.createElement('img');
+        image.src= dataItem.image_link
+        let name = document.createElement('p');
+        name.textContent = dataItem.name
+        let price = document.createElement('p');
+        price.textContent = dataItem.price
+        let rating = document.createElement('p');
+        rating.textcontent = dataItem.rating
+        let element = document.getElementById('result')
+        element.appendChild(image)
+        element.appendChild(name)
+        element.appendChild(price)
+        element.appendChild(rating)
+      
+      })
+      
+      })
+    })
