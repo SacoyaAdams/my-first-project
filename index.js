0945b84fe463045bafa2d08d2b7f47b69950907b
Sacoya <sacoyaengineer@gmail.com>
2023-01-16 18:26:43 -0500

    added domcontentloaded

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+  console.log("The DOM has loaded");
+
 const input = document.getElementById('new_search');
 document.querySelector('form').addEventListener('submit', (e) => {
     e.preventDefault()
@@ -28,3 +31,4 @@ input.addEventListener('click', function() {
       
       })
     })
+  })
